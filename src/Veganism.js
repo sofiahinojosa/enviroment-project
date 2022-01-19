b1e91d052fe3ddc27a2b4a288bb95d157e2d0597
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-01-18 22:12:36 -0600

    bai

--- src/Veganism.js
+++ src/Veganism.js
@@ -1,9 +1,28 @@
 import React from "react"
+import "./App.css"
+import "./Veganism.css"
 
 export default function Veganism() {
     return(
-        <div>
-            <h1>hello worlddddddddddddddd</h1>
+        <div className="Veganism">
+            <div className="main">
+            <h1>Veganism :]</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ridiculus, nullam habitant diam eget tincidunt ad nunc quisque, 
+                class enim risus odio dapibus dignissim torquent. Neque lacus sollicitudin velit conubia orci 
+                gravida parturient nunc nostra vel per vulputate at, 
+                montes dui proin mollis integer congue laoreet massa aenean in accumsan magnis. 
+                Accumsan scelerisque mus himenaeos natoque pharetra velit rhoncus id vivamus et.</p>
+                <img alt="bridge" src="https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+                <img alt="bridge" src="https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
+            <ul>
+                <li>hello</li>
+                <li>idk</li>
+                <li>hello</li>
+                <li>idk</li>
+                <li>hello</li>
+                <li>idk</li>
+            </ul>
+            </div>
         </div>
     )
 }
