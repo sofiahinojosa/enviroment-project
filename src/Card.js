494a038b785a97462c9b971f83423c91c3d92dbc
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-01-23 15:01:35 -0600

    ksfghkdjghgkdjsfg

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./Card.css";
 
-export default function Card() {
+export default function Card(props) {
     return(
         <div className="Card">
             <div className="grid-container">
                 <div className="grid-item">
-                    <img alt="bridge" src="https://images.unsplash.com/photo-1447752875215-b2761acb3c5d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8M3x8bmF0dXJlfGVufDB8fDB8fA%3D%3D&auto=format&fit=crop&w=500&q=60" />
-                    <p>lol que</p>
+                    <img alt="bridge" src={props.src} />
+                    <p>{props.text}</p>
                 </div>
             </div>
         </div>
--- src/Veganism.js
+++ src/Veganism.js
@@ -9,22 +9,26 @@ export default function Veganism() {
         <div className="Veganism">
             <div className="main">
             <h1>Veganism :]</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ridiculus, nullam habitant diam eget tincidunt ad nunc quisque, 
-                class enim risus odio dapibus dignissim torquent. Neque lacus sollicitudin velit conubia orci 
-                gravida parturient nunc nostra vel per vulputate at, 
-                montes dui proin mollis integer congue laoreet massa aenean in accumsan magnis. 
-                Accumsan scelerisque mus himenaeos natoque pharetra velit rhoncus id vivamus et.</p>
+            <p>There are many ways to <strong>embrace vegan living.</strong> Yet one thing all vegans have in common is a plant-based diet
+                 avoiding all animal foods such as meat (including fish, shellfish and insects), dairy, eggs and honey - 
+                 as well as avoiding animal-derived materials, products tested on animals and places that use animals for 
+                 entertainment.</p>
                 <div className="grid-container">
-                <Card /> <Card />
+                <Card src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/lentil-bolognese-1642711558.jpg?crop=0.896xw:0.598xh;0.104xw,0.225xh&resize=980:*" text="Vegan recipe from 'The Vegan Society'"/> <Card src="https://www.greenqueen.com.hk/wp-content/uploads/2020/01/lazy-cat-kitchen-vegan-larb.jpg" />
                 </div>
-            <ul>
-                <li>hello</li>
-                <li>idk</li>
-                <li>hello</li>
-                <li>idk</li>
-                <li>hello</li>
-                <li>idk</li>
-            </ul>
+                <h5>Begin planning your transition:</h5>
+            <ol>
+                <li>Go vegetarian and then move onto veganism</li>
+                <li>Remove all meat from your diet, including fish and poultry</li>
+                <li>Avoid products containing gelatin, rennet, and other animal products</li>
+                <li>Begin incorporating more whole grains, beans, legumes, tofu, nuts, and seeds into your diet</li>
+                
+            </ol>
+                <p>Vegans <strong>avoid exploiting animals for any purpose</strong>, with compassion being a key reason many 
+                 choose a vegan lifestyle. From accessories and clothing to makeup and bathroom items, animal products 
+                 and products tested on animals are found in more places than you might expect. Fortunately nowadays there 
+                 are <strong>affordable and easily-sourced alternatives to just about everything</strong>. With over 58,000 products and 
+                 services registered with our Vegan Trademark alone, <strong>living a vegan lifestyle has never been easier.</strong></p>
             </div>
         </div>
     )
