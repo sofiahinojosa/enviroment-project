522985ccfb626876ce17e482aebd3b78fec70caa
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-01-24 23:16:37 -0600

    a

---
+++ src/Hamburger.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Hamburger.css";
+
+export default function Hamburger() {
+    return(
+        <div className="Hamburger">
+            <div className="burger burger 1"></div>
+            <div className="burger burger 2"></div>
+            <div className="burger burger 3"></div>
+        </div>
+    )
+}
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,11 +1,22 @@
-import React from "react";
+import React, { useState }  from "react";
 import { Link } from "react-router-dom"; //add Routes, Route if needed
 import "./App.css"
+import Hamburger from "./Hamburger";
+import "./Hamburger.css";
 
 export default function Sidebar() {
+    const [hamburgerOpen, setHamburgerOpen] = useState(false);
+
+    const toggleHamburger = () =>{
+        setHamburgerOpen(!hamburgerOpen)
+    }
+
     return(
         <div className="Sidebar">
             <div class="sidenav">
+            <div className="Hamburger" onClick={toggleHamburger}>
+                <Hamburger />
+            </div>
                 <Link to="/"><h2>Prettygreenworld</h2></Link>
                 <hr />
                 <h3>Topics :)</h3>
