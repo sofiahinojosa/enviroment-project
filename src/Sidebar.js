a4eea32603aaaf3eeb5d00889607c01311ac2575
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-01-24 23:55:48 -0600

    gn

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,10 +13,10 @@ export default function Sidebar() {
 
     return(
         <div className="Sidebar">
-            <div class="sidenav">
             <div className="hamburger" onClick={toggleHamburger}>
                 <Hamburger />
             </div>
+            <div className="sidenav">
                 <Link to="/"><h2>Prettygreenworld</h2></Link>
                 <hr />
                 <h3>Topics :)</h3>
