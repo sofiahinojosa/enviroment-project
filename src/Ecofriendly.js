e64e89bfcde52a6101fdc270963c1835add323eb
Sofia Hinojosa <sofiahinojosadl@gmail.com>
2022-01-24 22:09:03 -0600

    a

--- src/Ecofriendly.js
+++ src/Ecofriendly.js
@@ -9,22 +9,35 @@ export default function Trees() {
         <div className="Veganism">
             <div className="main">
             <h1>Eco-friendly :]</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipiscing elit ridiculus, nullam habitant diam eget tincidunt ad nunc quisque, 
-                class enim risus odio dapibus dignissim torquent. Neque lacus sollicitudin velit conubia orci 
-                gravida parturient nunc nostra vel per vulputate at, 
-                montes dui proin mollis integer congue laoreet massa aenean in accumsan magnis. 
-                Accumsan scelerisque mus himenaeos natoque pharetra velit rhoncus id vivamus et.</p>
+            <p>It's surprisingly simple to make positive changes to your lifestyle
+                 just by figuring out where to start. Instead of using a K-Cup, swap
+                  it for a reusable coffee filter. Out of laundry detergent? Try one
+                   with better ingredients. You don't have to subsist on whatever your
+                    backyard garden grows to make a difference or feel guilty that your
+                     trash doesn't fit in a mason jar. Try to shop local when possible,
+                      only buy what you need, bike or walk or use public transportation 
+                      when able, fix what you own before replacing it</p>
                 <div className="grid-container">
-                <Card /> <Card />
+                <Card src="https://i.insider.com/6081d30a1041d50019f2db4f?width=700&format=jpeg&auto=webp" text="Swimwear made from recycled materials (Evarlane)" /> 
+                <Card src="https://i.insider.com/59f0ad198280a6bf118b4971?width=700&format=jpeg&auto=webp" text="An e-reader can help diminish the carbon footprint of someone who frequently buys new books" />
                 </div>
+                <h5>Eco-friendly ideas you should try:</h5>
             <ul>
-                <li>hello</li>
-                <li>idk</li>
-                <li>hello</li>
-                <li>idk</li>
-                <li>hello</li>
-                <li>idk</li>
+                <li>Reusable, organic cotton dish towels</li>
+                <li>Reusable grocery bags</li>
+                <li>Rechargeable batteries</li>
+                <li>Reusable water bottle</li>
+                <li>A showerhead that uses less water</li>
+                <li>Biodegradable trash bags</li>
             </ul>
+            <p>It's surprisingly simple to make positive changes to your lifestyle
+                 just by figuring out where to start. Instead of using a K-Cup, swap
+                  it for a reusable coffee filter. Out of laundry detergent? Try one
+                   with better ingredients. You don't have to subsist on whatever your
+                    backyard garden grows to make a difference or feel guilty that your
+                     trash doesn't fit in a mason jar. Try to shop local when possible,
+                      only buy what you need, bike or walk or use public transportation 
+                      when able, fix what you own before replacing it</p>
             </div>
         </div>
     )
